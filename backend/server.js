ad8da1951660adcbb1c01cd6ca17651a5ca0c69a
vuir <taevio@hotmail.com>
2025-12-01 00:47:13 +0300

    Add course route check

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./route/authRoutes");
+const courseRoutes = require("./route/courseRoutes");
 
 const app = express();
 
-connectDB();
-
 // middleware
-app.use(cors({ origin: "http://localhost:3000" }));  // Allow requests from frontend
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
-// auth routes
-const authRoutes = require("./route/authRoutes");  
-app.use("/api/auth", authRoutes); 
+connectDB();
 
-// test route
+// test
 app.get("/", (req, res) => {
-	res.json({ message: "backend is running well!" });
+    res.json({ message: "backend is running well!" });
 });
 
+// auth
+app.use("/api/auth", authRoutes);
+
+// courses
+app.use("/api/courses", courseRoutes);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+);
